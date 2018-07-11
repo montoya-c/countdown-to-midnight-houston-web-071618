f1aec000883f67de6981fe8477ad2aa51c166705
Cassandra Montoya <montoya.c@outlook.com>
2018-07-11 17:24:21 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,10 @@
 #write your code here
 
 def countdown (number)
-  number = 0
-  while number > 10
+  number = 10
+  while number > 0
   puts "#{number} SECOND(S)!"
-  
+  number -= 1
+end
   puts "HAPPY NEW YEAR!"
 end
